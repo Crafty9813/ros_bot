43eed7862ee3a64b462faa30133f1e1ad3953dca
jonathan <jonathanli668899@gmail.com>
2025-01-26 17:36:26 -0500

    jazzy working

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,11 +4,12 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
-from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -21,16 +22,31 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+
+    default_world = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds',
+        'empty.world'
+        )
+    
+    world = LaunchConfiguration('world')
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+        )
+
     # Include the Gazebo launch file, provided by the ros_gz package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                    launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
              )
 
     # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+                                   '-name', 'my_bot'],
                         output='screen')
 
 
@@ -48,6 +64,7 @@ def generate_launch_description():
     # Launch
     return LaunchDescription([
         rsp,
+        world_arg,
         gazebo,
         spawn_entity,
         ros_gz_bridge
