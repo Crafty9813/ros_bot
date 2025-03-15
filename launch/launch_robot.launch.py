25d3ccf8e0ff7698acc9fc9c57a4e8da32d40717
Jonathan <jonathanli668899@gmail.com>
2025-03-15 13:13:33 -0400

    im so stuck

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -39,7 +39,7 @@ def generate_launch_description():
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
 
@@ -52,7 +52,7 @@ def generate_launch_description():
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )
 
